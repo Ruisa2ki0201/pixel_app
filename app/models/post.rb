f01e837740c8f9ce68916dd4e587cf69e403d418
Ruisa2ki0201 <ruisa2ki0201@gmail.com>
2020-08-21 14:45:23 +0900

    修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   has_many :likes
-  has_many :liked_users, through: :likes, sourcee: :user
+  has_many :liked_users, through: :likes
 end
