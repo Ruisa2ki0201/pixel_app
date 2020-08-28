abafbba138d94ba15522acdd823f3c1d82afba59
Ruisa2ki0201 <ruisa2ki0201@gmail.com>
2020-08-28 21:12:58 +0900

    sns認証の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,22 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :liked_posts, through: :likes
   has_many :comments, dependent: :destroy
-  has_many :sns_credentils
+  has_many :sns_creadentials
   def already_liked?(post)
     self.likes.exists?(post_id: post.id)
+  end
+
+  def self.from_omniauth(auth)
+    sns = SnsCreadential.where(provider: auth.provider, uid: auth.uid).first_or_create
+    user = User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+      email: auth.info.email
+    )
+
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    { user: user, sns: sns }
   end
 end
