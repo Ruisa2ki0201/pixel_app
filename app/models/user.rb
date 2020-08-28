ceea12d3d11a270b4d07b895650095661739c879
Ruisa2ki0201 <ruisa2ki0201@gmail.com>
2020-08-28 14:57:38 +0900

    SnsCredentialテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
   has_many :posts
   has_many :likes, dependent: :destroy
   has_many :liked_posts, through: :likes
   has_many :comments, dependent: :destroy
+  has_many :sns_credentils
   def already_liked?(post)
     self.likes.exists?(post_id: post.id)
   end
