594caca7d55877398021c496db9a06ebe18d2960
Ruisa2ki0201 <ruisa2ki0201@gmail.com>
2020-09-04 11:16:44 +0900

    ユーザー編集機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,17 @@ class UsersController < ApplicationController
     @nickname = user.nickname
     @posts = current_user.posts
   end
+
+  def edit
+  end
+
+  def update
+    current_user.update(user_params)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :email, :image)
+  end
 end
