47f33a3f00049e6aaa33806c2af8d33116d630f3
Ruisa2ki0201 <ruisa2ki0201@gmail.com>
2020-09-04 13:54:40 +0900

    マイグレーションの削除

--- db/migrate/20200902095433_removegenre_idposts.rb
+++
@@ -1,9 +0,0 @@
-class RemovegenreIdposts < ActiveRecord::Migration[6.0]
-  def up
-    remove_column :posts, :genre_id
-  end
-
-  def down
-    remove_column :posts, :genre_id, :integer
-  end
-end
